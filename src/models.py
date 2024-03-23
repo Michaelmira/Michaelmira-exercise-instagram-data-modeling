768d5a5a13b822318e7cd05f790524a2a1da64f3
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-23 00:01:56 +0000

    bugfix

--- src/models.py
+++ src/models.py
@@ -1,16 +1,16 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Post Dislike(Base):
+class PostDislike(Base):
     __tablename__ = "post_dislike"
-    id = Column(integer, primary_key=True)
-    postID = Column(integer, ForeignKey("post.id"))
+    id = Column(Integer, primary_key=True)
+    postID = Column(Integer, ForeignKey("post.id"))
     user_id = Column(Integer, ForeignKey('user.id'))
     dislike = Column(Boolean, nullable=False)
     count = Column(Integer)
@@ -79,5 +79,5 @@ try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
